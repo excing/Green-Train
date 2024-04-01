6642b05321e46b9c33503ca11ba4be0784f2e34b
chshqiang <courage.exc@gmail.com>
2024-04-01 18:15:46 +0800

    Update: 经测试，vercel 不支持 websocket

--- vite.config.js
+++ vite.config.js
@@ -7,21 +7,3 @@ export default defineConfig({
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
 });
-
-import { Server } from "socket.io";
-
-const io = new Server({
-  cors: {
-    origin: "http://localhost:5173"
-  }
-})
-
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', '+ ' + msg);
-  });
-});
-
-io.listen(3000);
-
-console.log('Start WebSocket Server')
